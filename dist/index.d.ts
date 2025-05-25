8a376a2493a6d2ffc77a8aa1911613c8633806e0
TorresOwO <sergiotorres20000@gmail.com>
2025-05-26 00:50:23 +0200

    add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,82 @@
+import express, { Request, Response, Express } from 'express';
+import { Server as Server$1 } from 'https';
+import { Server } from 'http';
+
+type ExpressMiddleware = Parameters<express.Application['use']>[0];
+type sslFileRoute = {
+    keyPath: string;
+    certPath: string;
+};
+interface RequestT extends Request {
+    params: {
+        functionName: string;
+        [key: string]: string;
+    };
+}
+type ResponseT = Response<any, Record<string, any>>;
+type BodyProperty = {
+    type: 'object' | 'string' | 'number' | 'boolean';
+    properties?: Record<string, BodyProperty>;
+    required?: string[];
+    default?: any;
+};
+type TowersFunction = {
+    method: (req: RequestT, res: ResponseT, user?: any) => Promise<any>;
+    auth: boolean;
+    maxFiles?: number;
+    bodySchema?: BodyProperty;
+    rights?: {
+        [key: string]: string[];
+    };
+};
+type TowersExpressStartOptions = {
+    allowOrigin?: string;
+    onHttpStart?: (http: Server) => void;
+    onHttpsStart?: (https: Server$1) => void;
+};
+
+declare class TowersExpress {
+    app: Express;
+    httpsServer: Server$1 | undefined;
+    httpServer: Server;
+    private functionsEndpoint;
+    private port;
+    private sslPort;
+    private sslFiles;
+    constructor(functionsEndpoint: string, port: number);
+    configureSSL(sslPort: number, sslFiles: sslFileRoute): void;
+    /**
+     * Starts the Express server.
+     * @param allowOrigin - The allowed origin for CORS. Default is '*'.
+     */
+    start({ allowOrigin, onHttpsStart, onHttpStart }: TowersExpressStartOptions): void;
+    applyMiddleware(...args: any[]): void;
+    private openHttpServer;
+    private openSSLServer;
+}
+
+declare class TowersFunctionsController {
+    private static functions;
+    private static overridedCheckRights;
+    private static overridedAuthUser;
+    private static checkRights;
+    private static authUser;
+    /**
+     *
+     * @param func Function to check user rights.
+     * This function should return a string with error message if rights are not sufficient, or undefined if rights are sufficient.
+     */
+    static setCheckRightsFunction(func: (user: any, rights: TowersFunction['rights'], req: RequestT) => Promise<string | undefined>): void;
+    /**
+     *
+     * @param func Function to authenticate user.
+     * This function should return a user object or null/undefined if authentication fails.
+     */
+    static setAuthUserFunction(func: (req: RequestT) => Promise<any>): void;
+    static registerFunction(name: string, func: TowersFunction): void;
+    static getFunction(name: string): TowersFunction;
+    static listFunctions(): string[];
+    static callFunction(name: string, req: RequestT, res: ResponseT): Promise<any>;
+}
+
+export { type BodyProperty, type ExpressMiddleware, type RequestT, type ResponseT, TowersExpress, type TowersExpressStartOptions, type TowersFunction, TowersFunctionsController, type sslFileRoute };
---
+++ dist/index.js
@@ -0,0 +1,190 @@
+// src/expressUtils.ts
+import express from "express";
+
+// src/routes.ts
+import { Router } from "express";
+import multer from "multer";
+
+// src/functionsController.ts
+var TowersFunctionsController = class {
+  /**
+   * 
+   * @param func Function to check user rights.
+   * This function should return a string with error message if rights are not sufficient, or undefined if rights are sufficient.
+   */
+  static setCheckRightsFunction(func) {
+    this.checkRights = func;
+    this.overridedCheckRights = true;
+  }
+  /**
+   * 
+   * @param func Function to authenticate user.
+   * This function should return a user object or null/undefined if authentication fails.
+   */
+  static setAuthUserFunction(func) {
+    this.authUser = func;
+    this.overridedAuthUser = true;
+  }
+  static registerFunction(name, func) {
+    if (this.functions[name]) {
+      throw new Error(`Function ${name} is already registered.`);
+    }
+    this.functions[name] = func;
+  }
+  static getFunction(name) {
+    if (!this.functions[name]) {
+      throw new Error(`Function ${name} not found.`);
+    }
+    return this.functions[name];
+  }
+  static listFunctions() {
+    return Object.keys(this.functions);
+  }
+  static async callFunction(name, req, res) {
+    let func;
+    try {
+      func = this.getFunction(name);
+    } catch (error) {
+      console.error(`Error retrieving function ${name}:`, error);
+      res.status(404).send({ error: `Function not found: ${name}` });
+      return;
+    }
+    let user;
+    if (!this.overridedAuthUser) {
+      console.warn(`Using default auth user function for function ${name}. Consider overriding it for custom behavior.`);
+    }
+    user = await this.authUser(req);
+    if (func.auth) {
+      if (!user) {
+        res.status(401).send({ error: "Unauthorized" });
+        return;
+      }
+      if (func.rights) {
+        if (!this.overridedCheckRights) {
+          console.warn(`Using default rights check for function ${name}. Consider overriding it for custom behavior.`);
+        }
+        const checkRightsResult = await this.checkRights(user, func.rights, req);
+        if (checkRightsResult) {
+          res.status(403).send({ error: checkRightsResult });
+          console.warn(`User ${user.id} does not have rights for function ${name}: ${checkRightsResult}`);
+          return;
+        }
+      }
+    }
+    await func.method(req, res, user);
+  }
+};
+TowersFunctionsController.functions = {};
+TowersFunctionsController.overridedCheckRights = false;
+TowersFunctionsController.overridedAuthUser = false;
+TowersFunctionsController.checkRights = async (user, rights, req) => {
+  return void 0;
+};
+TowersFunctionsController.authUser = async (req) => {
+  return void 0;
+};
+
+// src/routes.ts
+var router = Router();
+var upload = multer({ storage: multer.memoryStorage() });
+router.post("/:functionName", (req, res, next) => {
+  const functionName = req.params.functionName;
+  let func;
+  try {
+    func = TowersFunctionsController.getFunction(functionName);
+  } catch (error) {
+    return;
+  }
+  if (func.maxFiles && func.maxFiles > 1) {
+    upload.array("files", func.maxFiles)(req, res, next);
+  } else {
+    upload.single("file")(req, res, next);
+  }
+}, async (req, res) => {
+  TowersFunctionsController.callFunction(req.params.functionName, req, res);
+});
+router.get("/:functionName", async (req, res) => {
+  req.body = req.query;
+  TowersFunctionsController.callFunction(req.params.functionName, req, res);
+});
+var routes_default = router;
+
+// src/expressUtils.ts
+import { createServer as createHttps } from "https";
+import { createServer as createHttp } from "http";
+import fs from "fs";
+var TowersExpress = class {
+  constructor(functionsEndpoint, port) {
+    this.openHttpServer = (onStart) => {
+      this.httpServer = createHttp(this.app);
+      if (onStart) {
+        onStart(this.httpServer);
+      }
+      this.httpServer.listen(this.port, () => {
+        return console.log(`Express is listening at http://localhost:${this.port}`);
+      });
+    };
+    this.openSSLServer = (onStart) => {
+      if (!this.sslFiles || !this.sslPort) {
+        console.error("SSL is not configured, call configureSSL first");
+        return;
+      }
+      try {
+        var options = {
+          key: fs.readFileSync(this.sslFiles.keyPath),
+          cert: fs.readFileSync(this.sslFiles.certPath)
+        };
+        const serverHttps = createHttps(options, this.app);
+        if (onStart) {
+          onStart(serverHttps);
+        }
+        serverHttps.listen(this.sslPort, () => {
+          return console.log(`Express is listening at https://localhost:${this.sslPort}`);
+        });
+        this.httpsServer = serverHttps;
+      } catch (error) {
+        console.error("Error reading SSL certificates: ", error);
+      }
+    };
+    this.app = express();
+    this.functionsEndpoint = functionsEndpoint.startsWith("/") ? functionsEndpoint : `/${functionsEndpoint}`;
+    this.port = port;
+  }
+  configureSSL(sslPort, sslFiles) {
+    this.sslPort = sslPort;
+    this.sslFiles = sslFiles;
+  }
+  /**
+   * Starts the Express server.
+   * @param allowOrigin - The allowed origin for CORS. Default is '*'.
+   */
+  start({
+    allowOrigin = "*",
+    onHttpsStart,
+    onHttpStart
+  }) {
+    this.applyMiddleware(express.json());
+    this.applyMiddleware(express.urlencoded({ extended: true }));
+    this.applyMiddleware((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", allowOrigin);
+      res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method");
+      res.header("Access-Control-Allow-Methods", "GET, POST");
+      res.header("Allow", "GET, POST");
+      next();
+    });
+    this.app.use(this.functionsEndpoint, routes_default);
+    this.app.get("*", (req, res) => {
+      res.sendStatus(404);
+    });
+    this.openSSLServer(onHttpsStart);
+    this.openHttpServer(onHttpStart);
+    console.log(`SERVER_PID=${process.pid}`);
+  }
+  applyMiddleware(...args) {
+    this.app.use(...args);
+  }
+};
+export {
+  TowersExpress,
+  TowersFunctionsController
+};
